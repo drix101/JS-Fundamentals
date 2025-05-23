702a69b4c8bec4079fc10ca3044dcd6a77267b52
Hendrix <hoyheenlawlarlawal@gmail.com>
2025-05-24 00:03:47 +0100

    print first argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,6 +1,8 @@
-const argsFigure = process.argv.slice(2);
+const argsFigure = process.argv(2);
 
 if (argsFigure === 0){
 
     console.log("No argument");
-};
+} else {
+    console.log('first argument');
+}
