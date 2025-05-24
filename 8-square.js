7649a4c2850205f0e919244348a9dc4708d7d650
Hendrix <hoyheenlawlarlawal@gmail.com>
2025-05-24 06:49:15 +0100

    print a square

--- 8-square.js
+++ 8-square.js
@@ -1,7 +1,7 @@
 const squareSize = parseInt(process.argv[2]);
 
 if (isNaN(squareSize)) {
-  console.log("Missing size");
+  console.log("Missing squareSize");
 } else {
   for (let i = 0; i < squareSize; i++) {
     console.log('X'.repeat(squareSize));
