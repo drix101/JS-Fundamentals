13ff23eccc90d0868e9934c423aee4814cd9c799
Hendrix <hoyheenlawlarlawal@gmail.com>
2025-05-24 07:04:04 +0100

    prints a factorial

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,12 +1,12 @@
-const  number = Number(process.argv[2]);
+const  num = Number(process.argv[2]);
 
 function factorial(n){
 
-    if (isNaN(n) || n < 1){
+    if (isNaN(n) || n <= 1){
 
         return 1;
     }
 
     return n * factorial(n - 1);
-        console.log(factorial(number));
+        console.log(factorial(num));
 }
