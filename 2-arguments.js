71a76d1eab08c7f1dd9e0b5b5e8b1781a501fca6
Hendrix <hoyheenlawlarlawal@gmail.com>
2025-06-02 12:15:08 +0100

    Javascript is amazing

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+const argsFigure = process.argv.slice(2);
+
+if (argsFigure.length === 0){
+
+    console.log("No argument");
+} else if (argsFigure.length === 1){
+    
+    console.log("Argument found");
+
+} else {
+    console.log("Arguments found");
+};
---
+++ 8-square.js
@@ -0,0 +1,9 @@
+const squareSize = parseInt(process.argv[2]);
+
+if (isNaN(squareSize)) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < squareSize; i++) {
+    console.log('X'.repeat(squareSize));
+  }
+}
